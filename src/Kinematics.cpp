c45efc353d3b3f1c934fb415c3db0a30fbd11f81
sasaki takeru <sasaki.takeru@gmail.com>
2020-03-14 12:13:41 +0900

    atan2

--- src/Kinematics.cpp
+++ src/Kinematics.cpp
@@ -9,9 +9,8 @@ float normalize_theta_range(float t){
 bool Kinematics::leg_ik(int sign, float x, float y, float l1, float l2, float *theta1, float *theta2)
 {
   if(sign!=-1 && sign!=1){ sign=1; }
-  float t1 = sign * acos((x*x+y*y+l1*l1-l2*l2)/(2*l1 * sqrt(x*x+y*y))) + atan(y/x);
-  if(x<0){ t1 += PI; }
-  float t2 = atan((y-l1 * sin(t1))/(x-l1 * cos(t1)))-t1;
+  float t1 = sign * acos((x*x+y*y+l1*l1-l2*l2)/(2*l1 * sqrt(x*x+y*y))) + atan2(y,x);
+  float t2 = atan2((y-l1 * sin(t1)),(x-l1 * cos(t1)))-t1;
   if(isnan(t1) || isnan(t2)){
     *theta1 = NAN;
     *theta2 = NAN;
